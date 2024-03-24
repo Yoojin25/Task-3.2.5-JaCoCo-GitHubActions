d155746fc8db5955625e424334c8ac26b14e3df0
Yoojin25 <144284694+Yoojin25@users.noreply.github.com>
2024-03-24 19:46:13 +0300

    билд, упавший из-за недостаточного покрытия.java

--- src/main/java/org/example/BoundaryService.java
+++ src/main/java/org/example/BoundaryService.java
@@ -3,6 +3,10 @@ package org.example;
 public class BoundaryService {
 
     public int findMin(int[] marks) {
+        if (marks == null || marks.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        
         int currentMin = marks[0];
         for (int mark : marks) {
             if (currentMin > mark) {
